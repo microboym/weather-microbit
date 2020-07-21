3a974fbfd9fac803abef543282f05d6325a08a06
microboym <microboym@outlook.com>
2020-07-21 16:00:49 +0800

    rename symbol name

--- main.ts
+++ main.ts
@@ -4,16 +4,16 @@ serial.setBaudRate(BaudRate.BaudRate9600)
 display.show(new display.spinning) // loading animation
 
 serial.onDataReceived(serial.delimiters(Delimiters.Fullstop), function () {
-    let data = serial.readUntil(serial.delimiters(Delimiters.Fullstop))
-    if (data == "Connect") {
+    let msg = serial.readUntil(serial.delimiters(Delimiters.Fullstop))
+    if (msg == "Connect") {
         display.show(new display.success)
         serial.writeLine("Connect successful")
-    } else if (data.substr(0, 7) == "Weather") {
-        display.stop()
-        basic.showString("data:" + data.substr(9))
+    } else if (msg.substr(0, 7) == "Weather") {
+        let d = msg.substr(9)
+        data.decode(d)
     } else {
         display.stop()
-        basic.showString("Unknow:" + data + " len=" + data.length, 75)
+        basic.showString("Unknow:" + msg + " len=" + msg.length, 75)
         // basic.showString("substr:" + data.substr(0, 7) + " len=" + data.substr(0, 7).length, 75)
     }
 })
